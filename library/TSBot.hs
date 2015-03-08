f017f6290d1ef7b632d042a29e9a7789a27c42d3
Remy Goldschmidt <taktoa@gmail.com>
2015-03-08 17:43:56 -0500

    Wrote initial telnet code

--- library/TSBot.hs
+++ library/TSBot.hs
@@ -1,10 +1,40 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
--- | TODO
+-- | The main TSBot library
 module TSBot (module TSBot) where
 
 -- GENERATE: import New.Module as TSBot
+import Control.Concurrent (forkIO, killThread)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Resource
+import Data.Conduit
+import Data.Conduit.Binary hiding (mapM_)
+import Network (connectTo, PortID (..))
+import System.Environment (getArgs, getProgName)
+import System.IO
 
--- | TODO
+-- | Telnet IO function
+telnet :: String -> Int -> IO ()
+telnet host port = runResourceT $ do
+  (releaseSock, hsock) <- allocate (connectTo host $ PortNumber $ fromIntegral port) hClose
+  liftIO $ mapM_ (`hSetBuffering` LineBuffering) [ stdin, stdout, hsock ]
+  (releaseThread, _) <- allocate (forkIO $
+                                  runResourceT $
+                                  sourceHandle stdin $$
+                                  sinkHandle hsock) killThread
+  sourceHandle hsock $$ sinkHandle stdout
+  release releaseThread
+  release releaseSock
+
+-- | Main function
 main :: IO ()
-main = return ()
+main = do
+    args <- getArgs
+    case args of
+        [host, port] -> telnet host (read port :: Int)
+        _ -> usageExit
+  where
+    usageExit = do
+        name <- getProgName
+        putStrLn $ "Usage : " ++ name ++ " host port"
+
