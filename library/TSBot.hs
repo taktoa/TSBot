39ed3da60fbe9d191644a08dcb9e2838e0c1420a
Remy Goldschmidt <taktoa@gmail.com>
2015-03-08 22:49:26 -0500

    Major fixes/changes, possibly switching to machines

--- library/TSBot.hs
+++ library/TSBot.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- | The main TSBot library
 module TSBot (module TSBot) where
@@ -8,33 +9,56 @@ import Control.Concurrent (forkIO, killThread)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Resource
 import Data.Conduit
+import Data.Machine
+import qualified Data.Conduit.Combinators as CC
 import Data.Conduit.Binary hiding (mapM_)
+import Data.Text as T
+import Data.Text (Text)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B (pack, unpack)
 import Network (connectTo, PortID (..))
 import System.Environment (getArgs, getProgName)
-import System.IO
+import System.IO (BufferMode (..), hSetBuffering, hClose, stdin)
 
--- | Telnet IO function
-telnet :: String -> Int -> IO ()
-telnet host port = runResourceT $ do
+type BS = ByteString
+
+data Telnet = Telnet { tHost :: String
+                     , tPort :: Int
+                     }
+
+-- | Default telnet settings for TSClientQuery
+defaultTelnet :: Telnet
+defaultTelnet = Telnet "localhost" 25639
+
+-- | Raw telnet IO function
+telnetRaw :: String -> Int -> Source IO BS -> Sink BS IO () -> IO ()
+telnetRaw host port src sink = runResourceT $ do
   (releaseSock, hsock) <- allocate (connectTo host $ PortNumber $ fromIntegral port) hClose
-  liftIO $ mapM_ (`hSetBuffering` LineBuffering) [ stdin, stdout, hsock ]
-  (releaseThread, _) <- allocate (forkIO $
-                                  runResourceT $
-                                  sourceHandle stdin $$
-                                  sinkHandle hsock) killThread
-  sourceHandle hsock $$ sinkHandle stdout
+  liftIO $ hsock `hSetBuffering` LineBuffering
+  (releaseThread, _) <- allocate (forkIO $ sourceHandle hsock $$ sink) killThread
+  liftIO $ src $$ sinkHandle hsock
   release releaseThread
   release releaseSock
 
+-- | Telnet wrapper (accepts/sends 'ByteString')
+telnetBS :: Telnet -> Source IO BS -> Sink BS IO () -> IO ()
+telnetBS (Telnet host port) = telnetRaw host port
+
+-- | Telnet wrapper (accepts/sends 'Text')
+telnetText :: Telnet -> Source IO Text -> Sink Text IO () -> IO ()
+telnetText t src sink = telnetBS t (src $= encode utf8) (decode utf8 =$ sink)
+
+-- | Telnet wrapper (accepts/sends 'String')
+telnetStr :: Telnet -> Source IO String -> Sink String IO () -> IO ()
+telnetStr t src sink = telnetText t (src $= T.pack) (T.unpack =$ sink)
+
+printSink :: Sink Text IO ()
+printSink = awaitForever (liftIO . putStrLn . T.unpack)
+
+readSrc :: Source IO Text
+readSrc = liftIO (stdin `hSetBuffering` LineBuffering)
+          >> sourceHandle stdin $= decode utf8
+
 -- | Main function
 main :: IO ()
-main = do
-    args <- getArgs
-    case args of
-        [host, port] -> telnet host (read port :: Int)
-        _ -> usageExit
-  where
-    usageExit = do
-        name <- getProgName
-        putStrLn $ "Usage : " ++ name ++ " host port"
-
+main = telnetText defaultTelnet readSrc printSink
