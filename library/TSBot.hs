89fbb0712d826f83d19339db9b814508efa5cc3d
Remy Goldschmidt <taktoa@gmail.com>
2015-03-09 16:49:30 -0500

    Non-code related fixes

--- library/TSBot.hs
+++ library/TSBot.hs
@@ -8,18 +8,20 @@ module TSBot (module TSBot) where
 import Control.Concurrent (forkIO, killThread)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Resource
-import Data.Conduit
-import qualified Data.Machine as M
-import Data.Conduit.Combinators (encodeUtf8, decodeUtf8)
-import qualified Data.Conduit.Combinators as CC
-import Data.Conduit.Binary hiding (mapM_)
-import Data.Text as T
-import Data.Text (Text)
 import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as B (pack, unpack)
-import Network (connectTo, PortID (..))
+import qualified Data.ByteString.Char8 as B
+import Data.Conduit
+import Data.Conduit.Attoparsec
+import Data.Conduit.Binary hiding (mapM_)
+import Data.Conduit.Combinators (decodeUtf8, encodeUtf8)
+import qualified Data.Conduit.Combinators as CC
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T (putStrLn)
+import Network (PortID (..), connectTo)
 import System.Environment (getArgs, getProgName)
-import System.IO (BufferMode (..), hSetBuffering, hClose, stdin)
+import System.IO (BufferMode (..), hClose, hSetBuffering, stdin)
+import TSBot.ClientQuery.Parse
 
 type BS = ByteString
 
@@ -34,7 +36,8 @@ defaultTelnet = Telnet "localhost" 25639
 -- | Raw telnet IO function
 telnetRaw :: String -> Int -> Source IO BS -> Sink BS IO () -> IO ()
 telnetRaw host port src sink = runResourceT $ do
-  (releaseSock, hsock) <- allocate (connectTo host $ PortNumber $ fromIntegral port) hClose
+  let telnetConn = connectTo host $ PortNumber $ fromIntegral port
+  (releaseSock, hsock) <- allocate telnetConn hClose
   liftIO $ hsock `hSetBuffering` LineBuffering
   (releaseThread, _) <- allocate (forkIO $ sourceHandle hsock $$ sink) killThread
   liftIO $ src $$ sinkHandle hsock
@@ -49,12 +52,30 @@ telnetBS (Telnet host port) = telnetRaw host port
 telnetText :: Telnet -> Source IO Text -> Sink Text IO () -> IO ()
 telnetText t src sink = telnetBS t (src $= encodeUtf8) (decodeUtf8 =$ sink)
 
+toText :: (Monad m) => Conduit String m Text
+toText = CC.map T.pack
+
+unText :: (Monad m) => Conduit Text m String
+unText = CC.map T.unpack
+
 -- | Telnet wrapper (accepts/sends 'String')
 telnetStr :: Telnet -> Source IO String -> Sink String IO () -> IO ()
-telnetStr t src sink = telnetText t (src $= (CC.map T.pack)) ((CC.map T.unpack) =$ sink)
+telnetStr t src sink = telnetText t (src $= toText) (unText =$ sink)
 
-printSink :: Sink Text IO ()
-printSink = awaitForever (liftIO . putStrLn . T.unpack)
+ioSink :: (a -> IO ()) -> Sink a IO ()
+ioSink f = awaitForever (liftIO . f)
+
+printSink :: Show a => Sink a IO ()
+printSink = ioSink print
+
+bsputSink :: Sink ByteString IO ()
+bsputSink = ioSink B.putStrLn
+
+tputSink :: Sink Text IO ()
+tputSink = ioSink T.putStrLn
+
+sputSink :: Sink String IO ()
+sputSink = ioSink putStrLn
 
 readSrc :: Source IO Text
 readSrc = liftIO (stdin `hSetBuffering` LineBuffering)
@@ -62,4 +83,4 @@ readSrc = liftIO (stdin `hSetBuffering` LineBuffering)
 
 -- | Main function
 main :: IO ()
-main = telnetText defaultTelnet readSrc printSink
+main = telnetText defaultTelnet readSrc (conduitParserEither responseP =$ printSink)
