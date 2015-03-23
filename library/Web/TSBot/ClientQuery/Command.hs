fd47835a4b700e464c73dc5489154a75ac24d175
Remy Goldschmidt <taktoa@gmail.com>
2015-03-23 18:01:56 -0500

    Trimming command list

--- library/Web/TSBot/ClientQuery/Command.hs
+++ library/Web/TSBot/ClientQuery/Command.hs
@@ -8,14 +8,10 @@ module Web.TSBot.ClientQuery.Command
        ( IsCommand
        , Command (..)
        , MSCommand (..)
-       , FTCommand (..)
        , BNCommand (..)
-       , CMCommand (..)
        , CLCommand (..)
        , CHCommand (..)
-       , MGCommand (..)
        , SVCommand (..)
-       , TKCommand (..)
        ) where
 
 import           Data.Data
@@ -25,44 +21,20 @@ data MSCommand = MSCHelp
                | MSCQuit
                | MSCUse
                | MSCCurrentSCHandlerId
-               | MSCHashPassword
-               | MSCPermOverview
                | MSCSendTextMessage
                | MSCWhoAmI
-               | MSCVerifyChannelPW
-               | MSCVerifyServerPW
-               | MSCSetClientChanGroup
-               deriving (Eq, Show, Read, Data, Typeable)
-
--- | File transfer commands
-data FTCommand = FTCCreateDir
-               | FTCDeleteFile
-               | FTCGetFileInfo
-               | FTCGetFileList
-               | FTCInitDownload
-               | FTCInitUpload
-               | FTCList
-               | FTCRenameFile
-               | FTCStop
                deriving (Eq, Show, Read, Data, Typeable)
 
 -- | Ban-related commands
 data BNCommand = BNCAdd
-               | BNCClient
                | BNCDel
                | BNCDelAll
                | BNCList
                deriving (Eq, Show, Read, Data, Typeable)
 
 -- | Client commands
-data CLCommand = CLCAddPerm
-               | CLCDbDelete
-               | CLCDbEdit
-               | CLCDbList
-               | CLCDelPerm
-               | CLCGetDbIdFromUId
+data CLCommand = CLCGetDbIdFromUId
                | CLCGetIds
-               | CLCGetNameFromDbId
                | CLCGetNameFromUId
                | CLCGetUIdFromClId
                | CLCKick
@@ -79,89 +51,37 @@ data CLCommand = CLCAddPerm
                deriving (Eq, Show, Read, Data, Typeable)
 
 -- | Channel commands
-data CHCommand = CHCAddPerm
-               | CHCClientAddPerm
-               | CHCClientDelPerm
-               | CHCClientPermList
-               | CHCConnectInfo
+data CHCommand = CHCConnectInfo
                | CHCCreate
                | CHCDelete
-               | CHCDelPerm
                | CHCEdit
-               | CHCGroupAdd
-               | CHCGroupAddPerm
-               | CHCGroupClientList
-               | CHCGroupDel
-               | CHCGroupDelPerm
-               | CHCGroupList
-               | CHCGroupPermList
                | CHCList
                | CHCMove
                | CHCPermList
                | CHCVariable
                deriving (Eq, Show, Read, Data, Typeable)
 
--- | Complaint commands
-data CMCommand = CMCAdd
-               | CMCDel
-               | CMCDelAll
-               | CMCList
-               deriving (Eq, Show, Read, Data, Typeable)
-
--- | Message commands
-data MGCommand = MGCAdd
-               | MGCDel
-               | MGCGet
-               | MGCList
-               | MGCUpdateFlag
-               deriving (Eq, Show, Read, Data, Typeable)
-
 -- | Server commands
 data SVCommand = SVCVariable
                | SVCConnectInfo
                | SVCConnHandlerList
-               | SVCGroupAdd
-               | SVCGroupAddClient
-               | SVCGroupAddPerm
-               | SVCGroupDel
-               | SVCGroupDelClient
-               | SVCGroupDelPerm
-               | SVCGroupClientList
-               | SVCGroupList
-               | SVCGroupPermList
-               | SVCGroupsByClientId
-               deriving (Eq, Show, Read, Data, Typeable)
-
--- | Token commands
-data TKCommand = TKCAdd
-               | TKCDel
-               | TKCList
-               | TKCUse
                deriving (Eq, Show, Read, Data, Typeable)
 
 class (Typeable a, Data a) => IsCommand a where {}
 
 instance IsCommand MSCommand
-instance IsCommand FTCommand
 instance IsCommand BNCommand
 instance IsCommand CLCommand
 instance IsCommand CHCommand
-instance IsCommand CMCommand
-instance IsCommand MGCommand
 instance IsCommand SVCommand
-instance IsCommand TKCommand
 
 -- | Command GADT
 data Command a where
   CommandMS :: MSCommand -> Command a
-  CommandFT :: FTCommand -> Command a
   CommandBN :: BNCommand -> Command a
   CommandCL :: CLCommand -> Command a
   CommandCH :: CHCommand -> Command a
-  CommandCM :: CMCommand -> Command a
-  CommandMG :: MGCommand -> Command a
   CommandSV :: SVCommand -> Command a
-  CommandTK :: TKCommand -> Command a
 
 deriving instance Typeable Command
 
@@ -169,12 +89,7 @@ deriving instance Data a => Data (Command a)
 
 instance Show (Command a) where
   show (CommandMS t) = show t
-  show (CommandFT t) = show t
   show (CommandBN t) = show t
   show (CommandCL t) = show t
   show (CommandCH t) = show t
-  show (CommandCM t) = show t
-  show (CommandMG t) = show t
   show (CommandSV t) = show t
-  show (CommandTK t) = show t
-  show _             = error "Failed pattern match when showing a (Command a)"
--- library/Web/TSBot/ClientQuery/Command/Boomerang.hs
+++ library/Web/TSBot/ClientQuery/Command/Boomerang.hs
@@ -28,56 +28,29 @@ import           Text.Boomerang.TH
 import           Web.TSBot.ClientQuery.Command
 
 $(makeBoomerangs ''MSCommand)
-$(makeBoomerangs ''FTCommand)
 $(makeBoomerangs ''BNCommand)
 $(makeBoomerangs ''CLCommand)
 $(makeBoomerangs ''CHCommand)
-$(makeBoomerangs ''CMCommand)
-$(makeBoomerangs ''MGCommand)
 $(makeBoomerangs ''SVCommand)
-$(makeBoomerangs ''TKCommand)
 
 msP :: StringBoomerang () (MSCommand :- ())
 msP = toP [ (rMSCHelp,               "help")
           , (rMSCQuit,               "quit")
           , (rMSCUse,                "use")
           , (rMSCCurrentSCHandlerId, "currentschandlerid")
-          , (rMSCHashPassword,       "hashpassword")
-          , (rMSCPermOverview,       "permoverview")
           , (rMSCSendTextMessage,    "sendtextmessage")
           , (rMSCWhoAmI,             "whoami")
-          , (rMSCVerifyChannelPW,    "verifychannelpassword")
-          , (rMSCVerifyServerPW,     "verifyserverpassword")
-          , (rMSCSetClientChanGroup, "setclientchannelgroup")
-          ]
-
-ftP :: StringBoomerang () (FTCommand :- ())
-ftP = toP [ (rFTCCreateDir,          "ftcreatedir")
-          , (rFTCDeleteFile,         "ftdeletefile")
-          , (rFTCGetFileInfo,        "ftgetfileinfo")
-          , (rFTCGetFileList,        "ftgetfilelist")
-          , (rFTCInitDownload,       "ftinitdownload")
-          , (rFTCInitUpload,         "ftinitupload")
-          , (rFTCList,               "ftlist")
-          , (rFTCRenameFile,         "ftrenamefile")
-          , (rFTCStop,               "ftstop")
           ]
 
 bnP :: StringBoomerang () (BNCommand :- ())
 bnP = toP [ (rBNCAdd,                "banadd")
-          , (rBNCClient,             "banclient")
           , (rBNCDel,                "bandel")
           , (rBNCDelAll,             "bandelall")
           , (rBNCList,               "banlist")
           ]
 
 clP :: StringBoomerang () (CLCommand :- ())
-clP = toP [ (rCLCAddPerm,            "clientaddperm")
-          , (rCLCDbDelete,           "clientdbdelete")
-          , (rCLCDbEdit,             "clientdbedit")
-          , (rCLCDbList,             "clientdblist")
-          , (rCLCDelPerm,            "clientdelperm")
-          , (rCLCGetDbIdFromUId,     "clientgetdbidfromuid")
+clP = toP [ (rCLCGetDbIdFromUId,     "clientgetdbidfromuid")
           , (rCLCGetIds,             "clientgetids")
           , (rCLCGetNameFromDbId,    "clientgetnamefromdbid")
           , (rCLCGetNameFromUId,     "clientgetnamefromuid")
@@ -96,64 +69,19 @@ clP = toP [ (rCLCAddPerm,            "clientaddperm")
           ]
 
 chP :: StringBoomerang () (CHCommand :- ())
-chP = toP [ (rCHCAddPerm,            "channeladdperm")
-          , (rCHCClientAddPerm,      "channelclientaddperm")
-          , (rCHCClientDelPerm,      "channelclientdelperm")
-          , (rCHCClientPermList,     "channelclientpermlist")
-          , (rCHCConnectInfo,        "channelconnectinfo")
+chP = toP [ (rCHCConnectInfo,        "channelconnectinfo")
           , (rCHCCreate,             "channelcreate")
           , (rCHCDelete,             "channeldelete")
-          , (rCHCDelPerm,            "channeldelperm")
           , (rCHCEdit,               "channeledit")
-          , (rCHCGroupAdd,           "channelgroupadd")
-          , (rCHCGroupAddPerm,       "channelgroupaddperm")
-          , (rCHCGroupClientList,    "channelgroupclientlist")
-          , (rCHCGroupDel,           "channelgroupdel")
-          , (rCHCGroupDelPerm,       "channelgroupdelperm")
-          , (rCHCGroupList,          "channelgrouplist")
-          , (rCHCGroupPermList,      "channelgrouppermlist")
           , (rCHCList,               "channellist")
           , (rCHCMove,               "channelmove")
-          , (rCHCPermList,           "channelpermlist")
           , (rCHCVariable,           "channelvariable")
-          ]
-
-cmP :: StringBoomerang () (CMCommand :- ())
-cmP = toP [ (rCMCAdd,                "complainadd")
-          , (rCMCDel,                "complaindel")
-          , (rCMCDelAll,             "complaindelall")
-          , (rCMCList,               "complainlist")
-          ]
-
-mgP :: StringBoomerang () (MGCommand :- ())
-mgP = toP [ (rMGCAdd,                "messageadd")
-          , (rMGCDel,                "messagedel")
-          , (rMGCGet,                "messageget")
-          , (rMGCList,               "messagelist")
-          , (rMGCUpdateFlag,         "messageupdateflag")
           ]
 
 svP :: StringBoomerang () (SVCommand :- ())
 svP = toP [ (rSVCVariable,           "servervariable")
           , (rSVCConnectInfo,        "serverconnectinfo")
           , (rSVCConnHandlerList,    "serverconnectionhandlerlist")
-          , (rSVCGroupAdd,           "servergroupadd")
-          , (rSVCGroupAddClient,     "servergroupaddclient")
-          , (rSVCGroupAddPerm,       "servergroupaddperm")
-          , (rSVCGroupDel,           "servergroupdel")
-          , (rSVCGroupDelClient,     "servergroupdelclient")
-          , (rSVCGroupDelPerm,       "servergroupdelperm")
-          , (rSVCGroupClientList,    "servergroupclientlist")
-          , (rSVCGroupList,          "servergrouplist")
-          , (rSVCGroupPermList,      "servergrouppermlist")
-          , (rSVCGroupsByClientId,   "servergroupsbyclientid")
-          ]
-
-tkP :: StringBoomerang () (TKCommand :- ())
-tkP = toP [ (rTKCAdd,                "tokenadd")
-          , (rTKCDel,                "tokendelete")
-          , (rTKCList,               "tokenlist")
-          , (rTKCUse,                "tokenuse")
           ]
 
 toP :: (Category cat, Monoid (cat a c)) => [(cat b c, cat a b)] -> cat a c
@@ -161,14 +89,10 @@ toP x = foldr1 (<>) $ map (uncurry (.)) x
 
 comP :: StringBoomerang () (Command a :- ())
 comP = xmaph CommandMS unCommandMS msP <>
-       xmaph CommandFT unCommandFT ftP <>
        xmaph CommandBN unCommandBN bnP <>
-       xmaph CommandCM unCommandCM cmP <>
        xmaph CommandCL unCommandCL clP <>
        xmaph CommandCH unCommandCH chP <>
-       xmaph CommandMG unCommandMG mgP <>
-       xmaph CommandSV unCommandSV svP <>
-       xmaph CommandTK unCommandTK tkP
+       xmaph CommandSV unCommandSV svP
 
 commandParse :: Text -> Either StringError (Command a)
 commandParse = parseString comP . unpack
@@ -183,17 +107,9 @@ unCommandMS :: Command a -> Maybe MSCommand
 unCommandMS (CommandMS t) = Just t
 unCommandMS _             = Nothing
 
-unCommandFT :: Command a -> Maybe FTCommand
-unCommandFT (CommandFT t) = Just t
-unCommandFT _             = Nothing
-
 unCommandBN :: Command a -> Maybe BNCommand
 unCommandBN (CommandBN t) = Just t
 unCommandBN _             = Nothing
-
-unCommandCM :: Command a -> Maybe CMCommand
-unCommandCM (CommandCM t) = Just t
-unCommandCM _             = Nothing
 
 unCommandCL :: Command a -> Maybe CLCommand
 unCommandCL (CommandCL t) = Just t
@@ -203,14 +119,6 @@ unCommandCH :: Command a -> Maybe CHCommand
 unCommandCH (CommandCH t) = Just t
 unCommandCH _             = Nothing
 
-unCommandMG :: Command a -> Maybe MGCommand
-unCommandMG (CommandMG t) = Just t
-unCommandMG _             = Nothing
-
 unCommandSV :: Command a -> Maybe SVCommand
 unCommandSV (CommandSV t) = Just t
 unCommandSV _             = Nothing
-
-unCommandTK :: Command a -> Maybe TKCommand
-unCommandTK (CommandTK t) = Just t
-unCommandTK _             = Nothing
