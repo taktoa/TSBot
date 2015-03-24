99d97db0e7a8953f0ad544e3dbecb4bc82ca8960
Remy Goldschmidt <taktoa@gmail.com>
2015-03-23 20:23:53 -0500

    Working on Command

--- library/Web/TSBot/ClientQuery/Command.hs
+++ library/Web/TSBot/ClientQuery/Command.hs
@@ -1,95 +1,243 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE GADTs              #-}
-{-# LANGUAGE StandaloneDeriving #-}
 
 -- | ClientQuery command types
-module Web.TSBot.ClientQuery.Command
-       ( IsCommand
-       , Command (..)
-       , MSCommand (..)
-       , BNCommand (..)
-       , CLCommand (..)
-       , CHCommand (..)
-       , SVCommand (..)
-       ) where
+module Web.TSBot.ClientQuery.Command where
 
 import           Data.Data
+import           Data.Natural
+import           Data.Text    (Text, pack, unpack)
 
--- | Miscellaneous commands
-data MSCommand = MSCHelp
-               | MSCQuit
-               | MSCUse
-               | MSCCurrentSCHandlerId
-               | MSCSendTextMessage
-               | MSCWhoAmI
-               deriving (Eq, Show, Read, Data, Typeable)
+data POrdering = LT | GT | EQ | NC
 
--- | Ban-related commands
-data BNCommand = BNCAdd
-               | BNCDel
-               | BNCDelAll
-               | BNCList
-               deriving (Eq, Show, Read, Data, Typeable)
+class Eq a => POrd a where
+  pcompare :: a -> a -> POrdering
 
--- | Client commands
-data CLCommand = CLCGetDbIdFromUId
-               | CLCGetIds
-               | CLCGetNameFromUId
-               | CLCGetUIdFromClId
-               | CLCKick
-               | CLCList
-               | CLCMove
-               | CLCMute
-               | CLCUnmute
-               | CLCNotifyRegister
-               | CLCNotifyUnregister
-               | CLCPermList
-               | CLCPoke
-               | CLCUpdate
-               | CLCVariable
-               deriving (Eq, Show, Read, Data, Typeable)
+data AType = AGetter | ASetter
 
--- | Channel commands
-data CHCommand = CHCConnectInfo
-               | CHCCreate
-               | CHCDelete
-               | CHCEdit
-               | CHCList
-               | CHCMove
-               | CHCPermList
-               | CHCVariable
-               deriving (Eq, Show, Read, Data, Typeable)
+class Fusible a where
+  aType   :: a -> AType
+  fusible :: a -> a -> Maybe a
 
--- | Server commands
-data SVCommand = SVCVariable
-               | SVCConnectInfo
-               | SVCConnHandlerList
-               deriving (Eq, Show, Read, Data, Typeable)
+class Render a where
+  render :: a -> Text
 
-class (Typeable a, Data a) => IsCommand a where {}
+data ChannelPropertyGet = ChPGName
+                        | ChPGTopic
+                        | ChPGDescription
+                        | ChPGCodec
+                        | ChPGCodecQuality
+                        | ChPGOrder
+                        | ChPGMaxClients
+                        | ChPGMaxFamilyClients
+                        | ChPGFlagPermanent
+                        | ChPGFlagSemiPermanent
+                        | ChPGFlagDefault
+                        | ChPGFlagPassword
+                        | ChPGFlagAreSubscribed
+                        | ChPGNamePhonetic
+                        | ChPGIconId
 
-instance IsCommand MSCommand
-instance IsCommand BNCommand
-instance IsCommand CLCommand
-instance IsCommand CHCommand
-instance IsCommand SVCommand
+data ChannelCodec = SpeexNarrowband
+                  | SpeexWideband
+                  | SpeexUltrawideband
+                  | CELTMono
+                  | OpusVoice
+                  | OpusMusic
+                  deriving (Eq, Ord, Enum, Show, Read)
 
--- | Command GADT
-data Command a where
-  CommandMS :: MSCommand -> Command a
-  CommandBN :: BNCommand -> Command a
-  CommandCL :: CLCommand -> Command a
-  CommandCH :: CHCommand -> Command a
-  CommandSV :: SVCommand -> Command a
+data ChannelCodecQuality = CCQ0 | CCQ1 | CCQ2 | CCQ3 | CCQ4
+                         | CCQ5 | CCQ6 | CCQ7 | CCQ8 | CCQ9
+                         | CCQ10
+                         deriving (Eq, Ord, Enum, Show, Read)
 
-deriving instance Typeable Command
+newtype NNatural = NNatural (Maybe Natural)
 
-deriving instance Data a => Data (Command a)
+newtype Flag = Flag Bool
 
-instance Show (Command a) where
-  show (CommandMS t) = show t
-  show (CommandBN t) = show t
-  show (CommandCL t) = show t
-  show (CommandCH t) = show t
-  show (CommandSV t) = show t
+data ChannelPropertySet = ChPSName              Text
+                        | ChPSTopic             Text
+                        | ChPSDescription       Text
+                        | ChPSCodec             ChannelCodec
+                        | ChPSCodecQuality      ChannelCodecQuality
+                        | ChPSOrder             Natural
+                        | ChPSMaxClients        NNatural
+                        | ChPSMaxFamilyClients  NNatural
+                        | ChPSFlagPermanent     Flag
+                        | ChPSFlagSemiPermanent Flag
+                        | ChPSFlagDefault       Flag
+                        | ChPSFlagPassword      Flag
+                        | ChPSFlagAreSubscribed Flag
+                        | ChPSNamePhonetic      Text
+                        | ChPSIconId            Natural
+
+data ClientPropertyGet = ClPGNickname
+                       | ClPGAway
+                       | ClPGAwayMessage
+                       | ClPGInputMuted
+                       | ClPGOutputMuted
+                       | ClPGNicknamePhonetic
+                       | ClPGFlagAvatar
+                       | ClPGMetaData
+
+type Hash = Text
+
+data ClientPropertySet = ClPSNickname         Text
+                       | ClPSAway             Flag
+                       | ClPSAwayMessage      Text
+                       | ClPSInputMuted       Flag
+                       | ClPSOutputMuted      Flag
+                       | ClPSNicknamePhonetic Text
+                       | ClPSFlagAvatar       Hash
+                       | ClPSMetaData         Text
+
+data Set a b = Set a b
+
+data RawCommand = RCQuit
+                | RCUse
+                | RCCurrentSCHandlerId
+                | RCSendTextMessage
+                | RCWhoAmI
+                | RCBanAdd
+                | RCBanDel
+                | RCBanDelAll
+                | RCBanList
+                | RCClientGetIds
+                | RCClientKick
+                | RCClientList
+                | RCClientMove
+                | RCClientMute
+                | RCClientUnmute
+                | RCClientNotifyRegister
+                | RCClientNotifyUnregister
+                | RCClientPermList
+                | RCClientPoke
+                | RCClientUpdate
+                | RCClientVariable
+                | RCChannelConnectInfo
+                | RCChannelCreate
+                | RCChannelDelete
+                | RCChannelEdit
+                | RCChannelList
+                | RCChannelMove
+                | RCChannelVariable
+                | RCServerVariable
+                | RCServerConnectInfo
+                | RCServerConnHandlerList
+                deriving (Eq, Show, Read, Data, Typeable)
+
+type UID = Natural
+type Time = Natural
+type BanID = Natural
+type ClientID = Natural
+type ClientUID = Text
+type SCHandlerID = Natural
+
+data Command = CQuit
+             | CWhoAmI
+             | CCurrentSCHandlerId
+             | CServerConnectInfo
+             | CServerConnHandlerList
+             | CChannelConnectInfo
+             | CBanList
+             | CClientList
+             | CChannelList
+             | CClientGetIds    ClientUID
+             | CUse             SCHandlerID
+             | CNotifyRegister  SCHandlerID Event
+             | CNotifyUnregister
+             | CPrvMessage      Msg         ClientID
+             | CChnMessage      Msg
+             | CSrvMessage      Msg
+             | CClientPoke      ClientID    Text
+             | CBanAdd          UID         Time      Text
+             | CBanDel          BanID
+             | CBanDelAll
+             | CClientKick      ClientID    Text
+             | CClientMove      ClientID    ChannelID
+             | CClientMute      ClientID
+             | CClientUnmute    ClientID
+             | CClientUpdate    ClPS
+             | CClientVariable  ClientID    ClPG
+             | CChannelCreate   Text        ChPS
+             | CChannelDelete   ChannelID
+             | CChannelEdit     ChannelID   ChPS
+             | CChannelMove     ChannelID   ChannelID
+             | CChannelVariable ChannelID   ChPG
+             | CServerVariable  SvPG
+             deriving (Eq, Show, Read, Data, Typeable)
+
+
+{-
+data Command = CQuit
+             | CUse
+             | CCurrentSCHandlerId
+             | CSendTextMessage
+             | CWhoAmI
+             | CBanAdd
+             | CBanDel
+             | CBanDelAll
+             | CBanList
+             | CClientGetDbIdFromUId
+             | CClientGetIds
+             | CClientGetNameFromUId
+             | CClientGetUIdFromClId
+             | CClientKick
+             | CClientList
+             | CClientMove
+             | CClientMute
+             | CClientUnmute
+             | CClientNotifyRegister
+             | CClientNotifyUnregister
+             | CClientPermList
+             | CClientPoke
+             | CClientUpdate
+             | CClientVariable
+             | CChannelConnectInfo
+             | CChannelCreate
+             | CChannelDelete
+             | CChannelEdit
+             | CChannelList
+             | CChannelMove
+             | CChannelVariable
+             | CServerVariable
+             | CServerConnectInfo
+             | CServerConnHandlerList
+             deriving (Eq, Show, Read, Data, Typeable)
+
+instance Fusible Command where
+  aType CQuit                   = ASetter
+  aType CUse                    = ASetter
+  aType CCurrentSCHandlerId     = AGetter
+  aType CSendTextMessage        = ASetter
+  aType CWhoAmI                 = AGetter
+  aType CBanAdd                 = ASetter
+  aType CBanDel                 = ASetter
+  aType CBanDelAll              = ASetter
+  aType CBanList                = AGetter
+  aType CClientGetDbIdFromUId   = AGetter
+  aType CClientGetIds           = AGetter
+  aType CClientGetNameFromUId   = AGetter
+  aType CClientGetUIdFromClId   = AGetter
+  aType CClientKick             = ASetter
+  aType CClientList             = AGetter
+  aType CClientMove             = ASetter
+  aType CClientMute             = ASetter
+  aType CClientUnmute           = ASetter
+  aType CClientNotifyRegister   = ASetter
+  aType CClientNotifyUnregister = ASetter
+  aType CClientPermList         = AGetter
+  aType CClientPoke             = ASetter
+  aType CClientUpdate           = ASetter
+  aType CClientVariable         = AGetter
+  aType CChannelConnectInfo     = AGetter
+  aType CChannelCreate          = ASetter
+  aType CChannelDelete          = ASetter
+  aType CChannelEdit            = ASetter
+  aType CChannelList            = AGetter
+  aType CChannelMove            = ASetter
+  aType CChannelVariable        = AGetter
+  aType CServerVariable         = AGetter
+  aType CServerConnectInfo      = AGetter
+  aType CServerConnHandlerList  = AGetter
+-}
